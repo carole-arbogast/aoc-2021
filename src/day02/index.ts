ef4f4ed6e66f7b9c1dc01f541f19f3d0881bdf4b
Carole Arbogast <arbogast.carole@gmail.com>
2021-12-12 11:37:13 +0200

    day2: solve part 2

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -9,7 +9,66 @@ forward 2`;
 
 type Direction = "forward" | "up" | "down";
 
+interface Position {
+  horizontal: number;
+  depth: number;
+}
+
+interface Movement {
+  direction: Direction;
+  distance: number;
+}
+
+interface ComplexPosition extends Position {
+  aim: number;
+}
+
 const parseInput = (rawInput: string) => rawInput;
+
+const calculateNewPosition1 = (position: Position, movement: Movement) => {
+  const { distance, direction } = movement;
+  const { horizontal, depth } = position;
+
+  const newHorizontal =
+    direction === "forward" ? horizontal + distance : horizontal;
+  const newDepth =
+    direction === "up"
+      ? depth - distance
+      : direction === "down"
+      ? depth + distance
+      : depth;
+
+  const newPosition = {
+    horizontal: newHorizontal,
+    depth: newDepth,
+  };
+
+  return newPosition;
+};
+
+const calculateNewPosition2 = (
+  position: ComplexPosition,
+  movement: Movement,
+) => {
+  const { direction, distance } = movement;
+  const { horizontal, depth, aim } = position;
+
+  const newHorizontal =
+    direction === "forward" ? horizontal + distance : horizontal;
+  const newDepth = direction === "forward" ? depth + aim * distance : depth;
+  const newAim =
+    direction === "up"
+      ? aim - distance
+      : direction === "down"
+      ? aim + distance
+      : aim;
+
+  return {
+    horizontal: newHorizontal,
+    depth: newDepth,
+    aim: newAim,
+  };
+};
 
 const formatInput = (input: string) => {
   const data: [Direction, number][] = input.split("\n").map((line) => {
@@ -31,19 +90,7 @@ const part1 = (rawInput: string) => {
   const finalPosition = formattedInput.reduce((acc, next) => {
     const [direction, distance] = next;
 
-    const horizontal =
-      direction === "forward" ? acc.horizontal + distance : acc.horizontal;
-    const depth =
-      direction === "up"
-        ? acc.depth - distance
-        : direction === "down"
-        ? acc.depth + distance
-        : acc.depth;
-
-    const newPosition = {
-      horizontal,
-      depth,
-    };
+    const newPosition = calculateNewPosition1(acc, { direction, distance });
 
     return (acc = newPosition);
   }, startingPosition);
@@ -54,8 +101,24 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const formattedInput = formatInput(input);
 
-  return;
+  const startingPosition = {
+    horizontal: 0,
+    depth: 0,
+    aim: 0,
+  };
+
+  const finalPosition = formattedInput.reduce((acc, next) => {
+    const [direction, distance] = next;
+
+    const newPosition = calculateNewPosition2(acc, { direction, distance });
+
+    return (acc = newPosition);
+  }, startingPosition);
+
+  const res = finalPosition.horizontal * finalPosition.depth;
+  return res;
 };
 
 run({
@@ -64,9 +127,7 @@ run({
     solution: part1,
   },
   part2: {
-    tests: [
-      // { input: ``, expected: "" },
-    ],
+    tests: [{ input: ``, expected: 900 }],
     solution: part2,
   },
   trimTestInputs: true,
